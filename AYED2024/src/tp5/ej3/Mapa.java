4513e6f900abb26b5a26815ce6b02fa9c04e54e0
Jack Sparrow <Jack Sparrow@192.168.0.4>
2024-06-05 23:53:01 -0300

    AyED

--- AYED2024/src/tp5/ej3/Mapa.java
+++ AYED2024/src/tp5/ej3/Mapa.java
@@ -20,7 +20,7 @@ public class Mapa<String> {
 	public List<String> devolverCamino(String ciudad1, String ciudad2){
 		List<String> camino = new ArrayList<String>();
 		List<String> temp = new ArrayList<String>();
-		boolean[] marca = new boolean[this.mapaCiudades.getSize()];
+		boolean[] marca = new boolean[this.mapaCiudades.getSize()+1];
 		int posCiudad1 = this.buscaPosDfs(ciudad1);
 		dfsModificado(posCiudad1,temp,camino,marca,ciudad2);
 		return camino;
@@ -37,7 +37,7 @@ public class Mapa<String> {
 			for(Edge<String> arista: aristas) {
 				Vertex<String> u = arista.getTarget();
 				if(!marca[u.getPosition()]) {
-					dfsModificado(pos,temp,camino,marca,ciudad2);
+					dfsModificado(u.getPosition(),temp,camino,marca,ciudad2);
 				}
 			}
 		}
@@ -46,7 +46,7 @@ public class Mapa<String> {
 	
 	private int buscaPosDfs(String ciudad){
 		int pos = 0;
-		boolean[] marca = new boolean[this.mapaCiudades.getSize()];		 
+		boolean[] marca = new boolean[this.mapaCiudades.getSize()+1];		 
 		for(int i=1;i<=marca.length;i++) {
 			if(!marca[i]) {
 				dfs(i,pos,marca,ciudad);
@@ -79,7 +79,7 @@ public class Mapa<String> {
 	public List<String> devolverCaminoExeptuando(String ciudad1, String ciudad2, List<String> ciudades){
 		List<String> camino = new ArrayList<String>();
 		List<String> temp = new ArrayList<String>();
-		boolean[] marca = new boolean[this.mapaCiudades.getSize()];
+		boolean[] marca = new boolean[this.mapaCiudades.getSize()+1];
 		if(!ciudades.contains(ciudad1)&&!ciudades.contains(ciudad2)) {
 			int posCiudad1 = this.buscaPosDfs(ciudad1);
 			for(int i=0; i<marca.length;i++) {
@@ -104,7 +104,7 @@ public class Mapa<String> {
 				excepcion = ciudades.contains(u.getData());
 				if(!excepcion) {
 					if(!marca[u.getPosition()]) {
-						dfsModificado(pos,temp,camino,marca,ciudad2);
+						dfsModificado(u.getPosition(),temp,camino,marca,ciudad2);
 					}
 				}
 			}
@@ -116,7 +116,7 @@ public class Mapa<String> {
 	
 	public List<String> caminoMasCorto(String ciudad1, String ciudad2){
 		List<String> lista = new ArrayList<String>();
-		int tamanio = this.mapaCiudades.getSize();
+		int tamanio = this.mapaCiudades.getSize()+1;
 		int[] pesos = new int[tamanio];
 		int[] previo = new int[tamanio];
 		boolean[] marca = new boolean[tamanio];
@@ -169,6 +169,87 @@ public class Mapa<String> {
 			}
 		}
 		return pos;
+	}                          
+	
+	// Ej3.inc4
+	
+	public List<String> caminoSinCargarCombustible(String ciudad1, String ciudad2, int tanque){
+		List<String> camino = new ArrayList<String>();
+		int posCiudad1 = buscaPosDfs(ciudad1);
+		int posCiudad2 = buscaPosDfs(ciudad2);
+		if(posCiudad1>0&&posCiudad2>0) {
+			List<String> temp = new ArrayList<String>();
+			boolean[] marca = new boolean[mapaCiudades.getSize()+1]; // lista comienza en 1?
+			dfsModificadoCombustible(posCiudad1,temp,camino,marca,ciudad2,tanque);
+		}
+		return camino;
 	}
 	
+	private void dfsModificadoCombustible(int pos, List<String> temp, List<String> camino, boolean[] marca, String ciudad2, int tanque) {
+		marca[pos]=true;
+		Vertex<String> v = mapaCiudades.getVertex(pos);
+		temp.add(v.getData());
+		if(v.getData().equals(ciudad2)) {
+			camino.addAll(temp);
+		} else {
+			List<Edge<String>> aristas = mapaCiudades.getEdges(v);
+			for(Edge<String> arista: aristas) {
+				Vertex<String> u = arista.getTarget();
+				if(!marca[u.getPosition()]) {
+					int combRestante = tanque-arista.getWeight();
+					if(combRestante>0) {
+						dfsModificadoCombustible(u.getPosition(),temp,camino,marca,ciudad2,combRestante);
+					}			
+				}
+			}
+		}
+	}
+	
+	// Ej3.inc4
+	
+	public List<String> caminoMenorCargaCombustible(String ciudad1, String ciudad2, int tanque){
+		List<String> camino = new ArrayList<String>();
+		int minimo = numGrande;
+		int cargas = 0;
+		int recarga = tanque;
+		int posCiudad1 = buscaPosDfs(ciudad1);
+		int posCiudad2 = buscaPosDfs(ciudad2);
+		if(posCiudad1>0&&posCiudad2>0) {
+			List<String> temp = new ArrayList<String>();
+			boolean[] marca = new boolean[mapaCiudades.getSize()+1]; // lista comienza en 1?
+			dfsModificadoMenorCarga(posCiudad1,temp,camino,marca,ciudad2,tanque,recarga,cargas,minimo);
+		}
+		return camino;
+	}
+	
+	private void dfsModificadoMenorCarga(int pos, List<String> temp, List<String> camino, boolean[] marca, String ciudad2, int tanque, int recarga, int cargas, int minimo) {
+		marca[pos]=true;
+		Vertex<String> v = mapaCiudades.getVertex(pos);
+		// temp.add(v.getData()); no se puede agregar aca xq no se en esta instancia si llego a v.
+		if(v.getData().equals(ciudad2)) {
+			if(cargas<minimo) {
+				camino.addAll(temp);
+				minimo = cargas;
+			}
+		} else {
+			//caso en que debo recargar
+			if(tanque==0) {
+				tanque=recarga;
+				cargas++;
+			}
+			List<Edge<String>> aristas = mapaCiudades.getEdges(v);
+			for(Edge<String> arista: aristas) {
+				Vertex<String> u = arista.getTarget();
+				if(!marca[u.getPosition()]) {
+					tanque = tanque-arista.getWeight();
+					if(tanque>=0) {
+						temp.add(u.getData()); // agrego u dado que verifique q llego con el tanque
+						dfsModificadoMenorCarga(u.getPosition(),temp,camino,marca,ciudad2,tanque,recarga,cargas,minimo);
+						temp.remove(temp.size()); // vuelvo la lista temp a su estado original para verificar cada vertice
+					}
+					tanque = tanque+arista.getWeight(); // vuelvo a setear el tanque en inicial, para verificar cada vertice
+				}
+			}
+		}
+	}
 }
