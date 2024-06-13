d2e1a3fec2bf35ffc8a7379b48d1d2eaa6fde933
fremn88 <84801472+fremn88@users.noreply.github.com>
2024-06-12 22:38:09 -0300

    Delete AYED2024/src/tp5 directory

--- AYED2024/src/tp5/ej1/Edge.java
+++
@@ -1,8 +0,0 @@
-package tp5.ej1;
-
-public interface Edge<T> {
-	public Vertex<T> getTarget();
-	public int getWeight();
-}
-
-
--- AYED2024/src/tp5/ej1/Vertex.java
+++
@@ -1,7 +0,0 @@
-package tp5.ej1;
-
-public interface Vertex<T> {
-	public T getData();
-	public void setData(T data);
-	public int getPosition();
-}
